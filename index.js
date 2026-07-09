9dcd8c8490ad382f28e81dcf784dcc0932a25f18
sigma-student-10 <mdafjolhossain20@gmail.com>
2026-07-09 20:18:24 +0600

    add new file

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("views engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded ({extended: true}));
+app.use(methodOverride("_method"));
 
 main()
 .then(() => {
@@ -22,7 +24,7 @@ async function main() {
 //Index Route
 app.get("/chats", async (req, res) => {
     let chats = await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs", {chats});
 });
 
@@ -61,6 +63,35 @@ let chat1 = new Chat({
 
 chat1.save().then((res) => {
     console.log(res);
+});
+
+//Edit Rout
+app.get("/chats/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+});
+
+//Update Rout
+app.put("/chats/:id", async (req, res) => {
+    let {id} = req.params;
+    let { msg: newMsg} = req.body;
+    console.log(newMsg);
+    let updatedChat = await Chat.findByIdAndUpdate(
+        id,
+         {msg: newMsg},
+         {runValidators: true, new: true });
+
+         console.log(updatedChat);
+         res.redirect("/chats");
+});
+
+//Destroy Route
+app.delete("/chats/:id", async (req, res) => {
+    let {id} = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id); 
+    console.log(deletedChat);
+    res.redirect("/chats");
 });
 
 app.get("/", (req, res) => {
